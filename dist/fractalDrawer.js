4cd6c8835ca629052f976dcc06df59ae03e2f46b
Arthur <arthrp@users.noreply.github.com>
2020-01-05 16:09:39 +0200

    Initial commit

---
+++ dist/fractalDrawer.js
@@ -0,0 +1,36 @@
+class FractalDrawer {
+    constructor(_canv, _ctx) {
+        this._canv = _canv;
+        this._ctx = _ctx;
+    }
+    draw(cReal) {
+        console.log(`drawing with ${cReal}`);
+        const cImaginary = -0.6;
+        const maxWidth = this._canv.width;
+        const maxHeight = this._canv.height;
+        const imageData = this._ctx.getImageData(0, 0, maxWidth, this._canv.height);
+        const data = imageData.data;
+        const maxIterations = 255;
+        let x = 0, y = 0;
+        for (var h = 0; h < maxHeight; h++) {
+            for (var w = 0; w < maxWidth; w++) {
+                x = -1.5 + w * 3.0 / maxWidth;
+                y = -1.0 + h * 2.0 / maxHeight;
+                let i = 0;
+                while ((x * x + y * y < 4) && (i < maxIterations)) {
+                    let xNew = x * x - y * y + cReal;
+                    let yNew = 2 * x * y + cImaginary;
+                    x = xNew;
+                    y = yNew;
+                    i++;
+                }
+                data[h * maxWidth * 4 + w * 4 + 0] = i * 25;
+                data[h * maxWidth * 4 + w * 4 + 1] = i * 5;
+                data[h * maxWidth * 4 + w * 4 + 2] = i * 8;
+                data[h * maxWidth * 4 + w * 4 + 3] = 255;
+            }
+        }
+        this._ctx.putImageData(imageData, 0, 0);
+    }
+}
+//# sourceMappingURL=fractalDrawer.js.map
---
+++ src/main.ts
@@ -0,0 +1,15 @@
+const canv = <HTMLCanvasElement> document.querySelector("#main-canv");
+const ctx = canv.getContext("2d");
+const fractalDrawer = new FractalDrawer(canv, ctx);
+let cReal = -0.4;
+
+document.querySelector("#c-real").addEventListener("input", (e) => {
+    let val = (<any>(e.srcElement)).value;
+    document.querySelector("#c-real-value").innerHTML = val;
+    cReal = val;
+
+    fractalDrawer.draw(parseFloat(val));
+});
+
+
+fractalDrawer.draw(-0.5);
